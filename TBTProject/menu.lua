2ebfcb5810a79cc3d5e19c22ab851f10c6428780
behistun <behistun@users.noreply.github.com>
2015-09-30 09:13:18 -0500

    Update menu.lua

--- TBTProject/menu.lua
+++ TBTProject/menu.lua
@@ -4,7 +4,7 @@
 --
 -----------------------------------------------------------------------------------------
 
-local composer = require( "composer" )
+local storyboard = require( "storyboard" )
 local scene = composer.newScene()
 
 -- include Corona's "widget" library
@@ -19,7 +19,7 @@ local playBtn
 local function onPlayBtnRelease()
 	
 	-- go to level1.lua scene
-	composer.gotoScene( "level1", "fade", 500 )
+	storyboard.gotoScene( "level1", "fade", 500 )
 	
 	return true	-- indicates successful touch
 end
