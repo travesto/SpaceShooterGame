c4cb944e041e1c24e373c491d3f9904c48a8c7ae
Ryan <jrl15b@acu.edu>
2015-10-01 12:10:52 -0500

    assets updates2
    uploaded dpad img
    upload scene and menu data

--- TBTProject/menu.lua
+++ TBTProject/menu.lua
@@ -19,7 +19,7 @@ local playBtn
 local function onPlayBtnRelease()
 	
 	-- go to game
-	composer.gotoScene( "gameplay", "fade", 500 )
+	composer.gotoScene( "scene1", "fade", 500 )
 	
 	return true	-- indicates successful touch
 end
